a06658ec5451dc0ebc2d456a20508d7f6240e21e
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-09-27 19:28:20 -0400

    Handle interaction and update game visuals

--- snake.py
+++ snake.py
@@ -25,3 +25,20 @@ def move():
         square(head.x, head.y, 9, 'red')
         update()
         return
+    
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
